b6721f4c642ed20ee9e4ebfc516bfa81959e1134
LisaALarson <lisalarsonll@gmail.com>
2015-08-03 08:31:08 -0500

    adds student info

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,23 +1,18 @@
 $(document).ready(function (){
 
     $.ajax({
-        //forward slash brings you to the server level
         url:"/data" ,
         success: function(data){
             console.log(data);
-            $.each(data, function(i){
-                $('.carousel').append('<div class="slide" data-id="'+i+'"></div>');//Add a slide to the carousel
-                $('.slide').last().append('<h2>'+this.name+'</h2>');//Add h2 name to the newly created slide
-                $('.slide').last().append('<h4>About ' + this.name + '<h4>');
-                $('.slide').last().append('<div class="description">'+this.description+'</div>');//Add decription to the newly created slide
-                $('.slide').last().append('<h4>Thanks ' + this.name + '<h4>');
-                $('.slide').last().append('<div class="shout-out">'+this.shoutOut+'</div>');//Add shoutout to the newly created slide
-                $('.slide').first().addClass('current');
-                //console.log(i + $('.slide').last().innerHeight());//get slide heights before positioning them as absolute
 
-                //$('.carousel-nav .arrow').css("display", "inline-block");//show the dot nav arrows once ajax is complete
-                //$('ul.nav-dots').append('<li class="dot" data-id="'+i+'">&bull;</li>');//Create a nav dot for each slide
-                //$('.nav-dots .dot').first().addClass('active');
+            $.each(data, function(i){
+                $('.carousel').append('<div class="slide" data-id="'+i+'"></div>');
+                $('.slide').last().append('<h2>'+this.name+'</h2>');
+                $('.slide').last().append('<h4>About ' + this.name + '<h4>');
+                $('.slide').last().append('<div class="description">'+this.description+'</div>');
+                $('.slide').last().append('<h4>Thanks ' + this.name + '<h4>');
+                $('.slide').last().append('<div class="shout-out">'+this.shoutOut+'</div>');
+                $('.slide').first().addClass('current');
             }); // end each function
 
             $('body').on('click', '.next', function(){
@@ -25,7 +20,7 @@ $(document).ready(function (){
                 var nextSlide = currentSlide.next();
                 if(nextSlide.length === 0){
                     nextSlide = $('.slide').first();
-                };
+                };//end if statement
                 currentSlide.removeClass('current');
                 nextSlide.addClass('current');
             });//end next click
@@ -35,21 +30,20 @@ $(document).ready(function (){
                 var prevSlide = currentSlide.prev();
                 if(prevSlide.length === 0){
                     prevSlide = $('.slide').last();
-                };
+                };//end if statement
                 currentSlide.removeClass('current');
                 prevSlide.addClass('current');
             });//end prev click
-
 
             setInterval(function(){
                 var currentSlide = $('.current');
                 var nextSlide = currentSlide.next();
                 if(nextSlide.length === 0){
                     nextSlide = $('.slide').first();
-                };
+                };//end if statement
                 currentSlide.removeClass('current');
                 nextSlide.addClass('current');
-            }, 3000);
+            }, 3000);//end interval function
 
 
         }//end success function
